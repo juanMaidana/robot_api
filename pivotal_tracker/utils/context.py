c6af85c467ff0ebf2ea279e33517dcadfe17281e
LGValdez <gabriel.valdez@jalasoft.com>
2019-08-15 12:33:35 -0400

    updated core tests simple OperatingSystem library test for later usage

--- pivotal_tracker/utils/context.py
+++ pivotal_tracker/utils/context.py
@@ -1,6 +1,7 @@
 class Context:
-    """Context for robot features
-    This class will get attributes according to the need on Test Cases
+    """Context for robot features.
+
+    This class will get attributes according to the need on Test Cases.
     """
     def __init__(self):
         pass
