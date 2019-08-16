16957e1c640e2f6ac80dbdfcc7d7d071337ffa6f
LGValdez <gabriel.valdez@jalasoft.com>
2019-08-16 09:17:19 -0400

    documentation on core files

--- core/libraries/my_library.py
+++ core/libraries/my_library.py
@@ -2,19 +2,27 @@ from os.path import join, realpath, dirname
 
 service_options = {
     "LINUX": ["mysql:5.7", "wordpress:latest"],
-    "WINDOWS": ["mysql:5.7", "wordpress:latest"]
+    "WINDOWS": ["mysql:5.7", "wordpress:latest"],
+    "OTHER": ["mysql:5.7", "wordpress:latest"]
 }
 
 
 def join_two_strings(arg1, arg2):
+    """Takes two strings and joins them."""
     return arg1 + " " + arg2
 
 
 def get_project_path():
+    """Using os library return the current project path"""
     return join(dirname(realpath(__file__)), '..', '..')
 
 
 def get_operating_system(environment_variables):
+    """Attempts to recognize the OS.
+
+    :param: environment_variables, dict or map of variables.
+    :returns: OS name recognized.
+    """
     if "SHELL" in environment_variables:
         return "LINUX"
     else:
@@ -22,5 +30,12 @@ def get_operating_system(environment_variables):
 
 
 def prepare_contents(operating_system, raw_data):
+    """Multiple operating systems might use different images.
+
+    Values will be replaced according to the service options map.
+    :param operating_system: (uppercase) can be linux, windows, other.
+    :param raw_data: data with values to be replaced.
+    :return: data with values replaced.
+    """
     replacer = service_options[operating_system]
     return raw_data.replace("${VAL_0}", replacer[0]).replace("${VAL_1}", replacer[1])
--- core/utils/request_wrapper.py
+++ core/utils/request_wrapper.py
@@ -2,18 +2,38 @@ from requests import request
 
 
 class RequestWrapper:
+    """Generic wrapper for HTTP requests."""
     def __init__(self):
+        """Attributes that will be used on basic functions.
+
+        By default,
+        The response of last request.
+        The endpoint of last request.
+        """
         self._response = None
         self._endpoint = None
 
     def set_end_point(self, value):
+        """Setter for endpoint attribute."""
         self._endpoint = value
 
     def do_request(self, http_type, headers=None, params=None, data=None):
-        self._response = request(http_type, self._endpoint, headers=headers, params=params, data=data)
+        """Execute the http request based on a type and given params.
+
+        :param http_type: GET, POST, PUT, DELETE.
+        :param headers: for request, by default no data required.
+        :param params: for request, by default no data required.
+        :param data: for request, by default no data required.
+        :return: True.
+        """
+        self._response = request(http_type, self._endpoint, headers=headers,
+                                 params=params, data=data)
+        return True
 
     def get_json(self):
+        """Getter for response as a json."""
         return self._response.json()
 
     def get_status(self):
+        """Get status code for last http request."""
         return str(self._response.status_code)
